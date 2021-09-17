0117c7c814c79c0f8445987a84807cc3e872a24f
Az1zK <90938782+Az1zK@users.noreply.github.com>
2021-09-17 16:36:07 -0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,54 @@
+#
+#   _____   _   _   _____   _   _        
+#|  ___| | | | | /  ___| | | / /       
+#| |__   | | | | | |     | |/ /        
+#|  __|  | | | | | |     | |\ \        
+#| |     | |_| | | |___  | | \ \       
+#|_|     \_____/ \_____| |_|  \_\
+#          Hacking Tools by Aziz.K 
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/fuckadmin/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Az1zK
+
+clear
+
+mkdir Az1zK
+
+clear
+
+cd Az1zK
+
+clear
+
+git clone https://github.com/Az1zK/fuckadmin.git
+
+clear
+
+cd fuckadmin
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
